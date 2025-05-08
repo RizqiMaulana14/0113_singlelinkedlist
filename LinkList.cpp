29025cd6b8459575ad982e3f266b212a4504ad16
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-08 15:31:56 +0700

    Bool Search dan List Empty

--- LinkList.cpp
+++ LinkList.cpp
@@ -34,8 +34,11 @@ class list{
             return;
         }
 
+        Node *current = START;
+        Node *previous = START;
+
         while ((current != NULL) && (nim >= current->noMhs)){
-            if( nim == current-noMhs){
+            if( nim == current->noMhs){
                 cout << "\nDuplikasi noMhs tidak diijinkan.\n";
                 return;
             }
@@ -45,6 +48,20 @@ class list{
 
         nodeBaru->next = current;
         previous->next = nodeBaru;
+    }
+    
+    bool listEmpty(){
+        return (START == NULL);
+    }
+
+    bool search (int nim, Node **previous, Node **current){
+        *previous = START;
+        *current = START;
+
+        while((*current != NULL) && (nim != (*current)->noMhs)){
+            *previous = *current;
+            *current = (*current)->next;
+        }
     }
 };
 
